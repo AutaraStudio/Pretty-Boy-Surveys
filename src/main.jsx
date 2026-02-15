6fdc448fd8a6b904fead6dd4b94a5cddaf1b75cc
Autara Studio <matt@autara.studio>
2026-02-15 17:45:25 +0000

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<div />} />
-      <Route path="/nps" element={<App />} />
-    </Routes>
+    <App />
   </BrowserRouter>
 )
